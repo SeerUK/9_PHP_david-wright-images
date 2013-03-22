a2965f0fd046bdbd2c5f19b503b96e9d2abf950b
Elliot Wright <wright.elliot@gmail.com>
2013-03-22 20:02:25 +0000

    GalleryCategory -> Gallery entity relationship

--- src/src/SeerUK/DWright/GalleryBundle/Controller/GalleryController.php
+++ src/src/SeerUK/DWright/GalleryBundle/Controller/GalleryController.php
@@ -18,28 +18,13 @@ class GalleryController extends Controller
 
     public function galleryAction($galleryId)
     {
-        try
-        {
-            $em = $this->getDoctrine()->getManager();
+        $em = $this->getDoctrine()->getManager();
 
-            $gallery = $em->getRepository('SeerUKDWrightGalleryBundle:Gallery')->findById($galleryId);
+        $gallery = $em->getRepository('SeerUKDWrightGalleryBundle:Gallery')->findById($galleryId);
 
-            return $this->render('SeerUKDWrightGalleryBundle:Gallery:gallery.html.twig', array(
-                'gallery'          => $gallery
-            ));
-        }
-        catch (\Doctrine\ORM\NoResultException $e)
-        {
-            return new Response($e->getMessage(), 404);
-        }
-        catch (\Doctrine\ORM\NonUniqueResultException $e)
-        {
-            return new Response($e->getMessage(), 400);
-        }
-        catch (\Exception $e)
-        {
-            return new Response($e->getMessage(), 500);
-        }
+        return $this->render('SeerUKDWrightGalleryBundle:Gallery:gallery.html.twig', array(
+            'gallery'          => $gallery
+        ));
     }
 
 
@@ -50,37 +35,22 @@ class GalleryController extends Controller
      */
     public function galleryCategoryAction($categoryId)
     {
-        try
-        {
-            $em = $this->getDoctrine()->getManager();
+        $em = $this->getDoctrine()->getManager();
 
-            $category  = $em->getRepository('SeerUKDWrightGalleryBundle:GalleryCategory')->findById($categoryId);
-            $galleries = $em->getRepository('SeerUKDWrightGalleryBundle:Gallery')->findByCategoryId($categoryId);
+        $category  = $em->getRepository('SeerUKDWrightGalleryBundle:GalleryCategory')->findById($categoryId);
+        $galleries = $category->getGalleries();
 
-            $galleryImages = [ ];
-            foreach ($galleries as $i => $gallery)
-            {
-                $galleryImages[$gallery->getId()] = $em->getRepository('SeerUKDWrightGalleryBundle:GalleryImage')
-                    ->findOneByGalleryId($gallery->getId());
-            }
+        $galleryImages = [ ];
+        foreach ($galleries as $i => $gallery)
+        {
+            $galleryImages[$gallery->getId()] = $em->getRepository('SeerUKDWrightGalleryBundle:GalleryImage')
+                ->findOneByGalleryId($gallery->getId());
+        }
 
-            return $this->render('SeerUKDWrightGalleryBundle:Gallery:category.html.twig', array(
-                'category'      => $category,
-                'galleries'     => $galleries,
-                'galleryImages' => $galleryImages
-            ));
-        }
-        catch (\Doctrine\ORM\NoResultException $e)
-        {
-            return new Response($e->getMessage(), 404);
-        }
-        catch (\Doctrine\ORM\NonUniqueResultException $e)
-        {
-            return new Response($e->getMessage(), 400);
-        }
-        catch (\Exception $e)
-        {
-            return new Response($e->getMessage(), 500);
-        }
+        return $this->render('SeerUKDWrightGalleryBundle:Gallery:category.html.twig', array(
+            'category'      => $category,
+            'galleries'     => $galleries,
+            'galleryImages' => $galleryImages
+        ));
     }
 }
