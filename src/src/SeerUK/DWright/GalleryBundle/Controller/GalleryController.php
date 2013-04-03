e26f7ed20cc348dd1cf3a8041ca3b445a95566f3
Elliot Wright <wright.elliot@gmail.com>
2013-04-03 21:05:11 +0100

    Started gallery view pages.

--- src/src/SeerUK/DWright/GalleryBundle/Controller/GalleryController.php
+++ src/src/SeerUK/DWright/GalleryBundle/Controller/GalleryController.php
@@ -8,27 +8,16 @@ use SeerUK\DWright\GalleryBundle\Entity\GalleryRepository;
 
 class GalleryController extends Controller
 {
-    public function indexaction()
-    {
-        return new Response('Test', 200);
-    }
-
-
     public function galleryAction($galleryId)
     {
-        $em = $this->getDoctrine()->getManager();
-
-        $gallery = $em->getRepository('SeerUKDWrightGalleryBundle:Gallery')->findById($galleryId);
+        $galleryHelper = $this->get('seer_ukd_wright_gallery.gallery_helper');
 
         return $this->render('SeerUKDWrightGalleryBundle:Gallery:gallery.html.twig', array(
-            'gallery' => $gallery
+            'gallery' => $galleryHelper->getPaginatedGalleryView($categoryId)
         ));
     }
 
 
-    /**
-     * @todo Add defaults to this for pagination
-     */
     public function galleryCategoryAction($categoryId)
     {
         $galleryHelper = $this->get('seer_ukd_wright_gallery.gallery_helper');
--- src/src/SeerUK/DWright/GalleryBundle/DependencyInjection/GalleryHelper.php
+++ src/src/SeerUK/DWright/GalleryBundle/DependencyInjection/GalleryHelper.php
@@ -1,26 +1,67 @@
 <?php
 
+/*
+ * This file is part of the SeerUKDwrightGalleryBundle
+ *
+ * (c) Elliot Wright <wright.elliot@gmail.com> - 2013
+ *
+ * For full license information please visit
+ * http://license.visualidiot.com/
+ */
+
 namespace SeerUK\DWright\GalleryBundle\DependencyInjection;
 
+/**
+ * Provides more flexible, decoupled access to Gallery related repositories
+ * and entities.
+ */
 class GalleryHelper
 {
-	/**
-	 * @var object
-	 */
-	protected $em;
+    /**
+     * @var object
+     */
+    protected $em;
 
 
-	public function __construct($em)
-	{
-		$this->em = $em;
-	}
+    /**
+     * @param [object] $em [An entity manager]
+     */
+    public function __construct($em)
+    {
+        $this->em = $em;
+    }
 
 
-	public function getPaginatedCategoryView($categoryId, $page = null, $perPage = null)
-	{
-		$category = $this->em->getRepository('SeerUKDWrightGalleryBundle:GalleryCategory')
-			->findGalleriesById($categoryId, $page, $perPage);
+    /**
+     * Returns a paginated category view page,
+     *
+     * @param  [integer] $categoryId [A category ID]
+     * @param  [integer] $page       [A page number]
+     * @param  [integer] $perPage    [A number of items to show per page]
+     * @return [object]              [Entity result set of category contents]
+     */
+    public function getPaginatedCategoryView($categoryId, $page = null, $perPage = null)
+    {
+        $category = $this->em->getRepository('SeerUKDWrightGalleryBundle:GalleryCategory')
+            ->findGalleriesById($categoryId, $page, $perPage);
 
-		return $category;
-	}
+        return $category;
+    }
+
+
+    /**
+     * Returns a paginated gallery view page,
+     *
+     * @param  [integer] $galleryId [A gallery ID]
+     * @param  [integer] $page      [A page number]
+     * @param  [integer] $perPage   [A number of items to show per page]
+     * @return [object]             [Entity result set of gallery contents]
+     */
+    public function getPaginatedGalleryView($galleryId, $page = null, $perPage = null)
+    {
+        $gallery = $this->em->getRepository('SeerUKDWrightGalleryBundle:Gallery')
+            ->findGalleriesById($categoryId, $page, $perPage);
+
+        return $gallery;
+    }
 }
