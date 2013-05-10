6b7eb428962313d4ffe76f6bdfa6bc5c6978ea69
Elliot Wright <wright.elliot@gmail.com>
2013-05-10 07:41:40 +0100

    Minor responsive tweaks, SQL updates.
    CSS:
    - Smaller screens benefit from the main header section taking up less
    screen space, but still being heroic enough.
    - Forms now have padding at the sides on tablet and mobile views.
    
    SQL:
    - Converted .sql files to user spaces instead of tabs.

--- sql/_gallery.sql
+++ sql/_gallery.sql
@@ -10,44 +10,44 @@ USE dwright;
 -- Table definitions:
 
 CREATE TABLE IF NOT EXISTS GalleryCategory (
-	intGalleryCategoryId int UNSIGNED NOT NULL AUTO_INCREMENT,
-	strGalleryCategoryName varchar(100) NOT NULL,
-	strGalleryCategoryDesc varchar(5000) NOT NULL,
-	stmTimestamp timestamp,
+    intGalleryCategoryId int UNSIGNED NOT NULL AUTO_INCREMENT,
+    strGalleryCategoryName varchar(100) NOT NULL,
+    strGalleryCategoryDesc varchar(5000) NOT NULL,
+    stmTimestamp timestamp,
 
-	PRIMARY KEY (intGalleryCategoryId)
+    PRIMARY KEY (intGalleryCategoryId)
 )
 ENGINE=InnoDB
 COMMENT="Portfolio gallery categories";
 
 
 CREATE TABLE IF NOT EXISTS Gallery (
-	intGalleryId int UNSIGNED NOT NULL AUTO_INCREMENT,
-	strGalleryName varchar(100) NOT NULL,
-	strGalleryDesc varchar(5000) NOT NULL,
-	intGalleryCategoryId int UNSIGNED NOT NULL,
-	intGalleryCoverImageId int UNSIGNED,
-	dtmPublished datetime NOT NULL,
-	stmTimestamp timestamp,
+    intGalleryId int UNSIGNED NOT NULL AUTO_INCREMENT,
+    strGalleryName varchar(100) NOT NULL,
+    strGalleryDesc varchar(5000) NOT NULL,
+    intGalleryCategoryId int UNSIGNED NOT NULL,
+    intGalleryCoverImageId int UNSIGNED,
+    dtmPublished datetime NOT NULL,
+    stmTimestamp timestamp,
 
-	PRIMARY KEY (intGalleryId),
-	FOREIGN KEY (intGalleryCategoryId) REFERENCES GalleryCategory (intGalleryCategoryId) ON UPDATE CASCADE ON DELETE CASCADE
+    PRIMARY KEY (intGalleryId),
+    FOREIGN KEY (intGalleryCategoryId) REFERENCES GalleryCategory (intGalleryCategoryId) ON UPDATE CASCADE ON DELETE CASCADE
 )
 ENGINE=InnoDB
 COMMENT="Portfolio galleries";
 
 
 CREATE TABLE IF NOT EXISTS GalleryImage (
-	intGalleryImageId int UNSIGNED NOT NULL AUTO_INCREMENT,
-	strGalleryImageName varchar(100) NOT NULL,
-	strGalleryImageDesc varchar(5000) NOT NULL,
-	intGalleryId int UNSIGNED NOT NULL,
-	strPath varchar(255) NOT NULL,
-	dtmPublished datetime NOT NULL,
-	stmTimestamp timestamp,
+    intGalleryImageId int UNSIGNED NOT NULL AUTO_INCREMENT,
+    strGalleryImageName varchar(100) NOT NULL,
+    strGalleryImageDesc varchar(5000) NOT NULL,
+    intGalleryId int UNSIGNED NOT NULL,
+    strPath varchar(255) NOT NULL,
+    dtmPublished datetime NOT NULL,
+    stmTimestamp timestamp,
 
-	PRIMARY KEY (intGalleryImageId),
-	FOREIGN KEY (intGalleryId) REFERENCES Gallery (intGalleryId) ON UPDATE CASCADE ON DELETE CASCADE
+    PRIMARY KEY (intGalleryImageId),
+    FOREIGN KEY (intGalleryId) REFERENCES Gallery (intGalleryId) ON UPDATE CASCADE ON DELETE CASCADE
 )
 ENGINE=InnoDB
 COMMENT="Portfolio gallery images";
--- sql/_user.sql
+++ sql/_user.sql
@@ -10,15 +10,16 @@ USE dwright;
 -- Table definitions:
 
 CREATE TABLE User (
-	intUserId int UNSIGNED NOT NULL AUTO_INCREMENT,
-	strUserName varchar(25) NOT NULL,
-	strPassword varchar(128) NOT NULL,
-	strEmail varchar(255) NOT NULL,
-	bolActive boolean NOT NULL,
+    intUserId int UNSIGNED NOT NULL AUTO_INCREMENT,
+    strUserName varchar(25) NOT NULL,
+    strPassword varchar(128) NOT NULL,
+    strEmail varchar(255) NOT NULL,
+    bolActive boolean NOT NULL,
+    stmTimestamp timestamp NOT NULL,
 
-	PRIMARY KEY (intUserId),
-	UNIQUE KEY (strUserName),
-	UNIQUE KEY (strEmail)
+    PRIMARY KEY (intUserId),
+    UNIQUE KEY (strUserName),
+    UNIQUE KEY (strEmail)
 )
 ENGINE=InnoDB,
 DEFAULT CHARSET utf8;
